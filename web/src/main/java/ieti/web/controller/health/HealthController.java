98adcdab5800b5547ff5539cf317bc4003599347
Julian <cisfjulian@gmail.com>
2023-09-09 08:16:39 -0500

    first commit

---
+++ web/src/main/java/ieti/web/controller/health/HealthController.java
@@ -0,0 +1,19 @@
+package ieti.web.controller.health;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+       @RequestMapping("/health")
+
+            public String checkAPI(){
+
+       return "<h1>The API is working ok!</h1>";
+
+   }
+
+
+}
